6d19a118e63a318673de22363ca67e1d29936243
Jordan Hoehne <33847459+Life-According-to-Jordan@users.noreply.github.com>
2018-02-18 17:04:56 -0600

    Update Twitter_Followers.py

--- Twitter_Followers.py
+++ Twitter_Followers.py
@@ -1,28 +1,44 @@
-#pip install python-twitter
-import twitter 
+"""
+virtualenv -p python 3 "folder_name" && cd "folder_name"
+source bin/activate
+python --version
+pip install requests bs4
+pip install python-twitter
+"""
+import twitter
 
 consumer_key= 'YOUR KEY'
 consumer_secret = 'YOUR CONSUMER SECRET'
 access_token = 'YOUR ACCESS TOKEN'
 access_secret = 'YOUR ACCESS SECRET'
 
-api = twitter.Api(consumer_key=consumer_key, 
+api = twitter.Api(consumer_key=consumer_key,
     consumer_secret = consumer_secret,
     access_token_key= access_token,
     access_token_secret=access_secret)
 
 print(api.VerifyCredentials())
 
-#get followers of user account 
+#get users and followers of user account
 followers = api.GetFollowers()
+#friends = api.GetFriends()
 
-#tweeting to my professor for fun while I code
+#tweeting to my professor
 #post_update = api.PostUpdates(status='@tyleransom tweet sent from python #Econ5970')
 #print(post_update)
 
-#pull all followers User Names 
+filename = "Followers.csv"
+f = open(filename, "w")
+headers = "User_Screen_Name, \n"
+f.write(headers)
+
+#pull all followers User Names
 for u in followers:
     print(u.screen_name)
+
+    f.write(u.screen_name + "\n")
+
+f.close
 
 
 
